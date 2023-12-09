de5977a34c56cb01f6d7faf42be3b814f9a7f666
Craig Allen <craig.c.allen@me.com>
2023-12-09 10:27:14 -0600

    updates to theme update process

--- update-check.php
+++ update-check.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 
 $theme_data = array(
     'version' => '1.0.1', // Your theme version
-    'url' => 'https://github.com/craigcallen/iprosystems/archive/master.zip', // URL to your ZIP file
+    'url' => 'https://github.com/craigcallen/iprosystems.com/archive/master.zip', // URL to your ZIP file
 );
 
 echo json_encode($theme_data);
